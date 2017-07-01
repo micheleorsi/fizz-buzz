e400e7821938edbaf568c110a93165d4c4633d00
Michele Orsi <michele.orsi@lastminute.com>
2017-07-01 17:04:32 +0100

    improved user output

--- src/main/java/it/micheleorsi/Main.java
+++ src/main/java/it/micheleorsi/Main.java
@@ -4,12 +4,18 @@ public class Main
 {
   public static void main(String[] args)
   {
-    new FizzBuzzApplication(
-      new FizzBuzzUseCase(
-        new MultiplierStrategy(3,"Fizz"),
-        new MultiplierStrategy(5,"Buzz")
-      ),
-      new FizzBuzzDisplay(System.out)
-    ).run(args);
+    try
+    {
+      new FizzBuzzApplication(
+        new FizzBuzzUseCase(
+          new MultiplierStrategy(3,"Fizz"),
+          new MultiplierStrategy(5,"Buzz")
+        ),
+        new FizzBuzzDisplay(System.out)
+      ).run(args);
+    } catch (RuntimeException ex)
+    {
+      System.out.println(ex.getMessage());
+    }
   }
 }
